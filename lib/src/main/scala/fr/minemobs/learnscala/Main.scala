741002ecbb75c290e7ceecf50c48ddd152920913
minemobs <issahakim94@gmail.com>
2021-03-30 14:06:31 +0200

    Added multilines strings

--- lib/src/main/scala/fr/minemobs/learnscala/Main.scala
+++ lib/src/main/scala/fr/minemobs/learnscala/Main.scala
@@ -2,7 +2,15 @@ package fr.minemobs.learnscala
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val username: String = new JMain().getUsername
+    val username: String = new JMain().getUsername //Immutable (can't be changed) use var if it's mutable
     println("Hello " + username)
+    val speech =
+      """This is a multiline
+        |String
+        |It's cool
+        |but i prefer
+        |adding a +
+        |.""".stripMargin
+        println(speech)
   }
 }
