47a2ce958d86f16350c6ff9330edde33f8680512
minemobs <issahakim94@gmail.com>
2021-03-30 17:19:05 +0200

    Added name list

--- lib/src/main/scala/fr/minemobs/learnscala/Main.scala
+++ lib/src/main/scala/fr/minemobs/learnscala/Main.scala
@@ -1,31 +1,68 @@
 package fr.minemobs.learnscala
 
 import java.io.PrintStream
+import scala.collection.mutable.ListBuffer
 import scala.io.StdIn.readLine
 import scala.util.control.Breaks.{break, breakable}
-import scala.util.{Random, Try}
+import scala.util.Random
 
 object Main {
   def main(args: Array[String]): Unit = {
     System.setOut(new PrintStream(System.out, true, "UTF-8"))
+    System.out.println("What's your name ?")
+    val username : String = readLine()
+    controlStructures(Seq(username))
+  }
+
+  def learnString(username: String): Unit = {
     /**
      * Val is immutable
      * Var is mutable
      */
-    //val username: String = new JMain().getUsername
-    System.out.println("What's your name ?")
-    val username = readLine()
+
     println(s"Hello $username")
-    val speech =
+    val speech : String =
       """This is a multiline
         |String
         |It's cool
         |but i prefer
         |adding a +
         |.""".stripMargin
-        println(speech)
+    println(speech)
+  }
 
-    moreOrLess()
+  def controlStructures(names: Seq[String]): Unit = {
+    val nameList = ListBuffer[String]() ++ names
+    breakable {
+      while (true) {
+        println(
+          """Do you want to check the name list or add a name to the list
+            |[0] View names
+            |[1] Add name
+            |[2] Quit""".stripMargin)
+        var intResponse: Int = 0
+        try {
+          intResponse = readLine().toInt
+        } catch {
+          case _: NumberFormatException =>
+            println("You didn't choose a number")
+            this.controlStructures(nameList.toSeq)
+        }
+
+        intResponse match {
+          case 0 =>
+            println("----------------------------------")
+            for (name <- nameList) println(name)
+            println("----------------------------------")
+
+          case 1 =>
+            println("Please write a name")
+            nameList.append(readLine())
+          case 2 => break();
+          case _ => this.controlStructures(nameList.toSeq)
+        }
+      }
+    }
   }
 
   def moreOrLess(): Unit = {
@@ -33,8 +70,8 @@ object Main {
     val rndNumber = new Random().nextInt(101)
     println(rndNumber)
     breakable{
-      for( a <- 1 to 5){
-        println("Choose a number")
+      for(_ <- 1 to 5){
+        println("Choisissez un nombre entre 0 et 100")
         val chosenNumber: Int = readLine().toInt
 
         if(chosenNumber == rndNumber){
